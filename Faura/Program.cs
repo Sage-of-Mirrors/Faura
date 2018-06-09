3bd63c161219d542ad1626cabf8fa5c629f8d10e
Sage_of_Mirrors <empressglatia@gmail.com>
2018-06-08 22:30:11 -0500

    Updated Program.cs to process the command arguments.

--- Faura/Program.cs
+++ Faura/Program.cs
@@ -10,8 +10,43 @@ namespace Faura
     {
         static void Main(string[] args)
         {
-            Event ev = new Event(args[0]);
+            string[] processedArgs = ProcessArgs(args);
+            Event ev = new Event(processedArgs[0], processedArgs[1], processedArgs[2]);
             //ShowHelp();
+        }
+
+        private static string[] ProcessArgs(string[] args)
+        {
+            string[] procArgs = new string[] { "", "", "" };
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (i + 1 >= args.Length)
+                    throw new Exception("The parameters were malformed.");
+
+                switch(args[i])
+                {
+                    case "-i":
+                    case "--input":
+                        procArgs[0] = args[i + 1];
+                        break;
+                    case "-o":
+                    case "--output":
+                        procArgs[1] = args[i + 1];
+                        break;
+                    case "-v":
+                    case "--version":
+                        procArgs[2] = args[i + 1];
+                        break;
+                    default:
+                        throw new Exception($"Unknown parameter \"{ args[i] }\"");
+                }
+
+                // Increment the counter to skip to the next parameter
+                i++;
+            }
+
+            return procArgs;
         }
 
         private static void ShowHelp()
